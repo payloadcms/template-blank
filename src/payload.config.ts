5eef5fef04fd5175e909459befac9bf56c5a87e6
Jessica Chowdhury <67977755+JessChowdhury@users.noreply.github.com>
2023-05-05 15:37:16 +0100

    feat: adds before dash component with next steps (#3)

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,10 +2,16 @@ import { buildConfig } from 'payload/config';
 import path from 'path';
 import Users from './collections/Users';
 import { payloadCloud } from '@payloadcms/plugin-cloud';
+import BeforeDashboard from './components/BeforeDashboard';
 
 export default buildConfig({
   admin: {
     user: Users.slug,
+    components: {
+      // The BeforeDashboard component renders the 'welcome' block that you see after logging into your admin panel.
+      // Feel free to delete this at any time. Simply remove the line below and the import BeforeDashboard statement on line 5.
+      beforeDashboard: [BeforeDashboard],
+    }
   },
   collections: [
     Users,
